84320ba027acb5d36aa0f6e4c224ca9b8d4480df
Krish-bohra <krishbohra194@gmail.com>
2026-04-19 19:45:15 +0530

    41

--- src/components/ui/social-icon.tsx
+++ src/components/ui/social-icon.tsx
@@ -8,40 +8,40 @@ const ButtonSocialIconDemo = () => {
       <Button
         variant="outline"
         type="button"
-        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-2"
+        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-3"
         onClick={() => window.open('https://twitter.com', '_blank')}
       >
-        <Twitter className="h-4 w-4" />
+        <Twitter className="h-6 w-6" />
       </Button>
 
       {/* Github */}
       <Button
         variant="outline"
         type="button"
-        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-2"
+        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-3"
         onClick={() => window.open('https://github.com/Krish-bohra', '_blank')}
       >
-        <Github className="h-4 w-4" />
+        <Github className="h-6 w-6" />
       </Button>
 
       {/* Linkedin */}
       <Button
         variant="outline"
         type="button"
-        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-2"
-        onClick={() => window.open('https://linkedin.com', '_blank')}
+        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-3"
+        onClick={() => window.open('https://www.linkedin.com/in/krish-bohra/', '_blank')}
       >
-        <Linkedin className="h-4 w-4 text-blue-500" />
+        <Linkedin className="h-6 w-6 text-blue-500" />
       </Button>
 
       {/* Instagram */}
       <Button
         variant="outline"
         type="button"
-        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-2"
-        onClick={() => window.open('https://instagram.com', '_blank')}
+        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-3"
+        onClick={() => window.open('https://www.instagram.com/itz_krish_bohra', '_blank')}
       >
-        <Instagram className="h-4 w-4 text-pink-500" />
+        <Instagram className="h-6 w-6 text-pink-500" />
       </Button>
     </div>
   );
