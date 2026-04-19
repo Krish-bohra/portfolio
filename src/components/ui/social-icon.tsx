9f672d76c7f8d02112369a519227331ae776afb6
Krish-bohra <krishbohra194@gmail.com>
2026-04-19 19:48:17 +0530

    42

--- src/components/ui/social-icon.tsx
+++ src/components/ui/social-icon.tsx
@@ -8,40 +8,52 @@ const ButtonSocialIconDemo = () => {
       <Button
         variant="outline"
         type="button"
-        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-3"
+        className="group flex items-center rounded-lg hover:scale-110 transition-all duration-300 cursor-pointer p-3 overflow-hidden"
         onClick={() => window.open('https://twitter.com', '_blank')}
       >
-        <Twitter className="h-6 w-6" />
+        <Twitter className="h-6 w-6 shrink-0" />
+        <span className="max-w-0 opacity-0 group-hover:max-w-[100px] group-hover:opacity-100 group-hover:px-1 group-hover:ml-2 transition-all duration-300 ease-out whitespace-nowrap text-sm font-medium">
+          Twitter
+        </span>
       </Button>
 
       {/* Github */}
       <Button
         variant="outline"
         type="button"
-        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-3"
+        className="group flex items-center rounded-lg hover:scale-110 transition-all duration-300 cursor-pointer p-3 overflow-hidden"
         onClick={() => window.open('https://github.com/Krish-bohra', '_blank')}
       >
-        <Github className="h-6 w-6" />
+        <Github className="h-6 w-6 shrink-0" />
+        <span className="max-w-0 opacity-0 group-hover:max-w-[100px] group-hover:opacity-100 group-hover:px-1 group-hover:ml-2 transition-all duration-300 ease-out whitespace-nowrap text-sm font-medium">
+          GitHub
+        </span>
       </Button>
 
       {/* Linkedin */}
       <Button
         variant="outline"
         type="button"
-        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-3"
+        className="group flex items-center rounded-lg hover:scale-110 transition-all duration-300 cursor-pointer p-3 overflow-hidden"
         onClick={() => window.open('https://www.linkedin.com/in/krish-bohra/', '_blank')}
       >
-        <Linkedin className="h-6 w-6 text-blue-500" />
+        <Linkedin className="h-6 w-6 text-blue-500 shrink-0" />
+        <span className="max-w-0 opacity-0 group-hover:max-w-[100px] group-hover:opacity-100 group-hover:px-1 group-hover:ml-2 transition-all duration-300 ease-out whitespace-nowrap text-sm font-medium">
+          LinkedIn
+        </span>
       </Button>
 
       {/* Instagram */}
       <Button
         variant="outline"
         type="button"
-        className="rounded-lg hover:scale-120 transition-all duration-300 cursor-pointer p-3"
+        className="group flex items-center rounded-lg hover:scale-110 transition-all duration-300 cursor-pointer p-3 overflow-hidden"
         onClick={() => window.open('https://www.instagram.com/itz_krish_bohra', '_blank')}
       >
-        <Instagram className="h-6 w-6 text-pink-500" />
+        <Instagram className="h-6 w-6 text-pink-500 shrink-0" />
+        <span className="max-w-0 opacity-0 group-hover:max-w-[100px] group-hover:opacity-100 group-hover:px-1 group-hover:ml-2 transition-all duration-300 ease-out whitespace-nowrap text-sm font-medium">
+          Instagram
+        </span>
       </Button>
     </div>
   );
